f5fe1a498f9458f14992b49dc23b8118429982e9
ETHAN-SONG914 <xingchen_24006343@utp.edu.my>
2025-06-20 18:00:33 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ typeEffect();
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
 }
+document.addEventListener('DOMContentLoaded', function() {
+    const title = document.getElementById("name-typing");
+    const text = title.textContent;
+    title.textContent = "";
+
+    function typeWriter(i) {
+        if (i < text.length) {
+            title.textContent += text.charAt(i);
+            i++;
+            setTimeout(() => typeWriter(i), 50); 
+        }
+    }
+    typeWriter(0); 
+});
